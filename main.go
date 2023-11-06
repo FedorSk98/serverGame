5b69563fdd4257d1bc4186fe040bb2aa4e800dfe
fa.skrynnikov <>
2023-11-06 23:30:37 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var clients = make(map[*websocket.Conn]bool) // Подключенные клиенты
+var broadcast = make(chan []byte)            // Канал сообщений
+var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+
+func main() {
+	// Настройка маршрута WebSocket
+	http.HandleFunc("/ws", handleConnections)
+
+	// Запуск обработки сообщений
+	go handleMessages()
+
+	// Запуск сервера на порту 8080
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Ошибка запуска сервера:", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Апгрейд HTTP-запроса в WebSocket
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal("Ошибка подключения:", err)
+	}
+
+	// Закрытие подключения при выходе из функции
+	defer ws.Close()
+
+	// Добавление клиента в список подключенных
+	clients[ws] = true
+
+	// Бесконечный цикл чтения сообщений с WebSocket
+	for {
+		var msg []byte
+		// Чтение сообщения
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.Printf("Ошибка чтения сообщения: %v", err)
+			delete(clients, ws)
+			break
+		}
+
+		log.Printf("чтение сообщения: %v", string(msg))
+		// Отправка сообщения в канал
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// Получение сообщения из канала
+		msg := <-broadcast
+
+		// Отправка сообщения всем подключенным клиентам
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				log.Printf("Ошибка отправки сообщения: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
